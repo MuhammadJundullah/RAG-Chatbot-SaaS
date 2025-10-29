30d095b5bd2ce54d0c5f1a32cae3bff144261fcb
Muhammad Jundullah <sayidmuhammad15@gmail.com>
2025-10-29 10:15:11 +0700

    fix: connection error postgre

--- app/core/database.py
+++ app/core/database.py
@@ -16,6 +16,7 @@ class DatabaseManager:
             pool_size=20, 
             max_overflow=10,
             pool_timeout=30,
+            connect_args={"statement_cache_size": 0},
         )
         self.async_session_maker = async_sessionmaker(
             self.engine, expire_on_commit=False, class_=AsyncSession
